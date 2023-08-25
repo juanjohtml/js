fb96bdb6e1ce137638a557371927a975cc3900d7
juanjo <juanjo.uwh@gmail.com>
2023-08-24 23:14:38 -0300

    segunda preentrega

--- main.js
+++ main.js
@@ -1,67 +1,41 @@
-//simulador de creditos
-//pedimos que ingrese al sistema
-let intro = prompt(
-  "Bienvenido!\n presione ENTER para acceder a un prestamo.\n presione 'S' para salir."
+
+//simulador de biblioteca
+
+let inicio = prompt(
+  "Bienvenido a la biblioteca, presione enter para acceder o salir para finalizar"
 );
-//bucle que valida la entrada
-while (intro == "") {
-  //ingreso de monto y cuotas
-  let monto = parseInt(prompt("ingrese el monto al que desea acceder"));
-  let cuotas = parseInt(
-    prompt(
-      "ingrese el numero de cuotas\n hasta 3 pagos %30 de interes. \n hasta 6 pagos %50 de interes. \n hasta 12 pagos %80 de interes.\n mas de 12 pagos %100 de interes."
-    )
+//declaracion array vacio
+const biblioteca = [];
+//declaracion funcion constructora
+function Libro(nombre, precio, stock = 0,id) {
+    this.nombre = nombre,
+    this.precio = precio,
+    this.stock = stock;
+    this.id = id = Libro.lenght+1
+}
+//ingreso 
+while (inicio != "salir") {
+  let accion = prompt(
+    `actualmente la biblioteca cuenta con ${biblioteca.length} libros.\n Ingrese la opcion deseada:\n 1- Agregar un libro\n 2- Consultar lista de libros \n 3- Eliminar un libro `
   );
-  //funcion para dividir el monto en las cuotas
-  function calcular(monto, cuotas) {
-    return monto / cuotas;
+  if (accion == 1) {
+    let nombreLibro = prompt("Ingrese el nombre del libro");
+    let precioLibro = prompt("Ingrese el precio del libro");
+    let cantidadLibro = prompt("Ingrese la cantidad de libros");
+    const libro1 =new Libro(nombreLibro,precioLibro,cantidadLibro);
+    biblioteca.push(libro1);
+    alert(`Libro ${libro1.nombre} agregado`)
+
+  }else if(accion==2){
+    alert(`libros de la biblioteca ${biblioteca}`)
   }
-  //condicional que verifica el numero de cuotas y agrega los porcentajes de intereses dando el resultado
-  if (cuotas <= 3) {
-    let montoInteres = monto + monto * 0.3;
-    let montoFinal = calcular(montoInteres, cuotas);
-    alert(
-      `Su prestamo de $${monto} fue aprobado y debera abonar ${cuotas} pagos de $${montoFinal.toFixed(
-        2
-      )}`
-    );
-  } else if (cuotas > 3 && cuotas <= 6) {
-    let montoInteres = monto + monto * 0.5;
-    let montoFinal = calcular(montoInteres, cuotas);
-    alert(
-      `Su prestamo de $${monto} fue aprobado y debera abonar ${cuotas} pagos de $${montoFinal.toFixed(
-        2
-      )}`
-    );
-  } else if (cuotas > 6 && cuotas <= 12) {
-    let montoInteres = monto + monto * 0.8;
-    let montoFinal = calcular(montoInteres, cuotas);
-    alert(
-      `Su prestamo de $${monto} fue aprobado y debera abonar ${cuotas} pagos de $${montoFinal.toFixed(
-        2
-      )}`
-    );
-  } else if (cuotas > 12) {
-    let montoInteres = monto + monto;
-    let montoFinal = calcular(montoInteres, cuotas);
-    alert(
-      `Su prestamo de $${monto} fue aprobado y debera abonar ${cuotas} pagos de $${montoFinal.toFixed(
-        2
-      )}`
-    );
-  } else {
-    alert("debe ingresar un numero de cuotas valido para continuar");
-  }
-  //finalizador o continuacion del bucle
-  intro = prompt(
-    "Desea calcular un nuevo prestamo?\n presione ENTER para acceder a un prestamo.\n presione 'S' para salir."
+
+  inicio = prompt(
+    "si desea realizar otra accion en la biblioteca presione ENTER o escriba salir para finalizar"
   );
 }
 
-/*
-  pedir monto
-  pedir cuotas
-  sumar porcentaje de intereses
-  calcular cuotas
-  devolver mensaje con cuotas y monto
-  */
+//ingresar libros a biblioteca
+//mostrar libros
+//buscar libros
+//eliminar libros
